519d5d114ed7e25aba0f372317db7b901d90d33a
RaphaelSM <raphaelnsx@gmail.com>
2021-12-24 17:37:39 -0300

    adição de funcionalidades
    adição da caixa suspensa de novo curso e início de alguns eventos no JS.

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => {
+    clearFields()
+    document.getElementById('modal').classList.remove('active')
+}
+
+
+
+// Eventos
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
+
+document.getElementById('salvar')
+    .addEventListener('click', saveClient)
+
+document.querySelector('#tableClient>tbody')
+    .addEventListener('click', editDelete)
+
+document.getElementById('cancelar')
+    .addEventListener('click', closeModal)
