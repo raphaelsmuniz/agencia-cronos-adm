8ba04beb251bd3e492790c610cd929bbac8e22c9
RaphaelSM <raphaelnsx@gmail.com>
2021-12-26 21:34:59 -0300

    CRUD
    CRUD criado.

--- script.js
+++ script.js
@@ -2,5 +2,31 @@
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_curso')) ?? []
 const setLocalStorage = (dbCurso) => localStorage.setItem('db_curso', JSON.stringify(dbCurso))
 
-//CRUD
+//CRUD -> create, read, update e delete
+const createCurso = (curso) => {
+    const dbCurso = getLocalStorage()
+    dbCurso.push (curso)
+    setLocalStorage(dbCurso)
+}
+
+const readCurso = () => getLocalStorage()
+
+const updateCurso = (index, curso) => {
+    const dbCurso = readCurso()
+    dbCurso[index] = curso
+    setLocalStorage(dbCurso)
+}
+
+const deleteCurso = (index) => {
+    const dbCurso = readCurso()
+    dbCurso.splice(index, 1)
+    setLocalStorage(dbCurso)
+}
+
+//Interação com o Layout
+
+const validaCampo = () => {
+    return document.getElementById('form').reportValidity()
+}
+
 
